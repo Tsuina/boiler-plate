288de007dee5cc5e8f1f7ce09a8d8f80ae0eaad1
Tsuina <gwenaeloppitz@gmail.com>
2023-11-16 17:43:17 +0100

    configured prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-
 function App() {
+  const check = 2;
   return (
     <>
       <div className="flex h-[100vh]">
-        <div className="m-auto p-[100px] text-[24px] text-yellow bg-dark ">
-
-          allok
+        <div className="m-auto p-[100px] text-[24px] text-yellow bg-dark">
+          {'allok?'}
         </div>
       </div>
     </>
