7419a65c2c22347b1e1d14ac5ae149a11c9d3b51
Tsuina <gwenaeloppitz@gmail.com>
2024-01-25 20:01:08 +0100

    added comment

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,10 @@ import ReactDOM from 'react-dom/client';
 import App from './App.js';
 import './input.css';
 
+// test
+
 ReactDOM.createRoot(document.getElementById('root') as Element).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 );
