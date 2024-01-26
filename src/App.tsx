de58abe704d702aabd44754424ae477ef7950fa9
Tsuina <gwenaeloppitz@gmail.com>
2024-01-26 08:51:37 +0100

    adding a test div

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,7 @@ function App() {
     <>
       <div className="flex h-[100vh]">
         <div className="m-auto max-w-[1024px]">
+          <div>adding a test div</div>
           <ShortResume />
         </div>
       </div>
